17a7c378c0039928681cdfc8618b2ee678862e9a
Ravi Kumar <ravikumar.off66@gmail.com>
2023-10-22 02:44:00 +0530

    model_window/starting

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+document.querySelector(".show-modal").addEventListener("click", function () {
+  document.querySelector("p").textContent;
+  console.log(document.querySelector("p").textContent);
+});
