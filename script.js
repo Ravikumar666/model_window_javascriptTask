874ab294157d1dbdf23b8f94a3a968ffb3ea68ad
Ravi Kumar <ravikumar.off66@gmail.com>
2023-10-22 04:26:45 +0530

    done

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-"use strict";
+'use strict';
 
-document.querySelector(".show-modal").addEventListener("click", function () {
-  document.querySelector("p").textContent;
-  console.log(document.querySelector("p").textContent);
-});
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+const openModel = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const hiddenModel = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++)
+  btnShowModal[i].addEventListener('click', openModel);
+
+btnCloseModal.addEventListener('click', hiddenModel);
+overlay.addEventListener('click', hiddenModel);
